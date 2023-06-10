d7e3bbba8a477bdab058cf3b746ec17e2a8f7549
Anahi Salto <asaltoc1600@alumno.ipn.mx>
2023-06-10 14:03:41 -0600

    Ejercicio 10 finalizado

--- PracticaAlgoritmos/prac10.c
+++ PracticaAlgoritmos/prac10.c
@@ -8,5 +8,29 @@
 int main() {
     system("color D0");
     printf("\tI N I C I O  D E L  P R O G R A M A\n\n");
+    int numNumeros = 10;
+    int numeros[numNumeros];
+    printf("Por favor introduzca los numeros enteros...\n");
+    for(int i = 0; i<numNumeros; i++){
+        printf("Numero %2d: ",i+1);
+        scanf("%d",&numeros[i]);
+    }
+
+    int ordenar;
+    do{
+        ordenar = 1;
+        for(int i = 0; i<(numNumeros - 1); i++){
+            if(numeros[i] < numeros[i + 1]){
+                int aux = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = aux;
+                ordenar = 0;
+            }
+        }
+    }while(ordenar == 0);
+    printf("\nNumeros ordenados:\n");
+    for(int i = 0; i<numNumeros; i++){
+        printf("Numero %2d: %d\n",i+1,numeros[i]);
+    }
     return 0;
 }

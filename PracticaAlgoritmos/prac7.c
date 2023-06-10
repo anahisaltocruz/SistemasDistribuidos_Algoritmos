24a8f0f45db6538cc131613464bd97c3b8197524
Anahi Salto <asaltoc1600@alumno.ipn.mx>
2023-06-10 13:10:53 -0600

    Ejercicio 7 finalizado

--- PracticaAlgoritmos/prac7.c
+++ PracticaAlgoritmos/prac7.c
@@ -8,6 +8,27 @@
 
 int main() {
     system("color D0");
+    char oracion[100];
+    char opc;
     printf("\tI N I C I O  D E L  P R O G R A M A\n\n");
+    printf("Ingrese una cadena: ");
+    fgets(oracion, sizeof(oracion), stdin);
+    printf("\n¿Que desea realizar?\n");
+    printf("    a. Convertir todos los caracteres a mayusculas\n");
+    printf("    b. Convertir todos los caracteres a minusculas\n");
+    printf("Opcion: ");
+    scanf("%c",&opc);
+    if(opc == 'a'){
+        for(int i = 0; i<strlen(oracion); i++){
+            oracion[i] = toupper(oracion[i]);
+        }
+        printf("R E S U L T A D O: %s\n\n",oracion);
+    }else if(opc == 'b'){
+        for(int i = 0; i<strlen(oracion); i++){
+            oracion[i] = tolower(oracion[i]);
+        }
+        printf("R E S U L T A D O: %s\n\n",oracion);
+    }else
+        printf("Opcion no valida\n\n");
     return 0;
 }

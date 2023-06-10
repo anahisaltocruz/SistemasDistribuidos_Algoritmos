46af6a8198b100da73dd0274658943ec03e85b9f
Anahi Salto <asaltoc1600@alumno.ipn.mx>
2023-06-10 13:29:28 -0600

    Ejercicio 8 terminado

--- PracticaAlgoritmos/prac8.c
+++ PracticaAlgoritmos/prac8.c
@@ -8,5 +8,19 @@
 int main() {
     system("color D0");
     printf("\tI N I C I O  D E L  P R O G R A M A\n\n");
+    int numNumeros = 3;
+    int numeros[numNumeros];
+    printf("Por favor introduzca los numeros enteros...\n");
+    for(int i = 0; i<numNumeros; i++){
+        printf("Numero %d: ",i+1);
+        scanf("%d",&numeros[i]);
+    }
+    int numMayor = 0;
+    for(int i = 0; i<numNumeros; i++){
+        if(numeros[i] > numMayor){
+            numMayor = numeros[i];
+        }
+    }
+    printf("El numero mayor es: %d\n\n",numMayor);
     return 0;
 }
